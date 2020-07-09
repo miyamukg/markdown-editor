f89e652c07678cdd1bc3167cd8477e56521fb1bb
miyamukg <5myamuk1@gmail.com>
2020-07-09 20:59:55 +0900

    test

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Toolkit.Parsers.Markdown;
+using Microsoft.Toolkit.Parsers.Markdown.Blocks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            String beforeText = ((TextBox)sender).Text;
+            UpdateMDResult();
+        }
+        private void UpdateMDResult()
+        {
+            var document = new MarkdownDocument();
+            document.Parse("This is **Markdown**");
+
+            var json = JsonConvert.SerializeObject(document, Formatting.Indented, new StringEnumConverter());
+            textBox2.Text = json;
+        }
+    }
+}
